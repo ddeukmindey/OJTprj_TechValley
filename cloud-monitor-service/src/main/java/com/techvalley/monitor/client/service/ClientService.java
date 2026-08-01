24d47e99b3bc115e368100900dd82fe66e242e69
rabituckle <luongvietluong@gmail.com>
2026-08-01 22:01:18 +0700

    feat: client

---
+++ cloud-monitor-service/src/main/java/com/techvalley/monitor/client/service/ClientService.java
@@ -0,0 +1,22 @@
+package com.techvalley.monitor.client.service;
+
+import com.techvalley.monitor.client.dto.request.ClientRequest;
+import com.techvalley.monitor.client.dto.response.*;
+import com.techvalley.monitor.instance.Instance;
+
+import java.util.List;
+
+public interface ClientService {
+
+    ClientResponse createClient(ClientRequest request);
+
+    PageResponse<ClientResponse> getClients(int page, int size, String search);
+
+    List<Instance> getClientInstances(Long id);
+
+    ClientCostResponse getClientCost(Long id);
+
+    ClientCostForecastResponse getClientCostForecast(Long id);
+
+    ClientSlaResponse getClientSla(Long id);
+}
---
+++ cloud-monitor-service/src/main/java/com/techvalley/monitor/common/security/UserContext.java
@@ -0,0 +1,17 @@
+package com.techvalley.monitor.common.security;
+
+public class UserContext {
+    private static final ThreadLocal<UserContextInfo> CONTEXT = new ThreadLocal<>();
+
+    public static void set(UserContextInfo info) {
+        CONTEXT.set(info);
+    }
+
+    public static UserContextInfo get() {
+        return CONTEXT.get();
+    }
+
+    public static void clear() {
+        CONTEXT.remove();
+    }
+}
